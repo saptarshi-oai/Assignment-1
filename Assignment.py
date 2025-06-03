225194c68ec1b46d5b5a8a7f972a3219af88b67b
saptarshi-oai <saptarshipatikar@gmail.com>
2025-06-03 17:10:02 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,22 @@
+
+#Task 1: Perform Basic Mathematical Operations
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+
+
+
+#Task 2: Create a Personalized Greeting
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+fullname = firstname + " " + lastname
+print("Hello, " + fullname + "! Welcome to the program.")
